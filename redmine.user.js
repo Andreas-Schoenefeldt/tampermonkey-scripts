89e08aa7226aac810a398c1f93d16ba07d1be17b
Andreas Schönefeldt <1157580+Andreas-Schoenefeldt@users.noreply.github.com>
2026-08-24 16:58:04 +0200

    Create redmine.user.js

---
+++ redmine.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Redmine Improvements
+// @namespace    https://redmine.flowconcept.de/
+// @version      2026-08-24
+// @description  Add improvements for redmine for faster work
+// @author       Andreas Schönefeldt
+// @match        https://redmine.flowconcept.de/*
+// @updateURL    https://github.com/Andreas-Schoenefeldt/tampermonkey-scripts/blob/main/redmine.user.js
+// @downloadURL  https://github.com/Andreas-Schoenefeldt/tampermonkey-scripts/blob/main/redmine.user.js
+// @icon         https://redmine.flowconcept.de/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function (){
+  console.log('loaded');
+})()
